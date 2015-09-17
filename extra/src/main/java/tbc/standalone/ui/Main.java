07bca1cd3772600f46901e8f213b402e101617a1
Zero Ryogi <zeroryogi@gmail.com>
2015-09-17 07:25:57 -0500

    stand

--- extra/src/main/java/tbc/standalone/ui/Main.java
+++ extra/src/main/java/tbc/standalone/ui/Main.java
@@ -24,17 +24,7 @@ public class Main extends Application {
     private Gson gson;
 
     public Main() {
-        // Creates the json object which will manage the information received
-        GsonBuilder builder = new GsonBuilder();
 
-        // Register an adapter to manage the date types as long values
-        builder.registerTypeAdapter(Date.class, new JsonDeserializer<Date>() {
-            public Date deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-                return new Date(json.getAsJsonPrimitive().getAsLong());
-            }
-        });
-
-        gson = builder.create();
     }
 
     @Override
@@ -43,43 +33,11 @@ public class Main extends Application {
         primaryStage.setTitle("Tbc/Embeded System");
         primaryStage.setScene(new Scene(root, 850, 610));
         primaryStage.show();
-
-        ArrayList<MoviBusVehiculo> a = new ArrayList<MoviBusVehiculo>(getMoviBus());
-        System.out.println("vcubs " + a.size());
     }
 
 
     public static void main(String[] args) {
         launch(args);
-    }
-
-    public List<Vcub> getVcubs() {
-        String response = Request.httpGetJsonRequest(HttpConstants.HOSTNAME + "/vcubs");
-        Type token = new TypeToken<Collection<Vcub>>() {
-        }.getType();
-        Collection<Vcub> result = gson.fromJson(response, token);
-
-        return new ArrayList<Vcub>(result);
-
-    }
-
-    public List<TranviaVehiculo> getTranvias() {
-        String response = Request.httpGetJsonRequest(HttpConstants.HOSTNAME + "/tranvias");
-        Type token = new TypeToken<Collection<TranviaVehiculo>>() {
-        }.getType();
-        Collection<TranviaVehiculo> result = gson.fromJson(response, token);
-
-        return new ArrayList<TranviaVehiculo>(result);
-
-    }
-
-    public List<MoviBusVehiculo> getMoviBus() {
-        String response = Request.httpGetJsonRequest(HttpConstants.HOSTNAME + "/mobibuses");
-        Type token = new TypeToken<Collection<MoviBusVehiculo>>() {
-        }.getType();
-        Collection<MoviBusVehiculo> result = gson.fromJson(response, token);
-
-        return new ArrayList<MoviBusVehiculo>(result);
     }
 
 }
--- extra/src/main/java/tbc/standalone/ui/controllers/Controller.java
+++ extra/src/main/java/tbc/standalone/ui/controllers/Controller.java
@@ -1,15 +1,23 @@
 package tbc.standalone.ui.controllers;
 
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import tbc.standalone.http.HttpConstants;
+import tbc.standalone.http.Request;
 import tbc.standalone.models.MoviBusVehiculo;
 import tbc.standalone.models.TranviaVehiculo;
 import tbc.standalone.models.Vcub;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
 
 
 public class Controller {
@@ -25,31 +33,71 @@ public class Controller {
     @FXML
     private ListView<TranviaVehiculo> tranviaList;
 
-    private ObservableList<String> tranviasItems;
+    private ObservableList<TranviaVehiculo> tranviasItems;
 
     private ArrayList<TranviaVehiculo> tranvias;
 
     @FXML
     private ListView<MoviBusVehiculo> movibusList;
 
-    private ObservableList<MoviBusVehiculo> MovisItems;
+    private ObservableList<MoviBusVehiculo> movisItems;
 
     private ArrayList<MoviBusVehiculo> mobis;
+
+    private Gson gson;
 
     @FXML
     public void initialize() {
 
+        // Creates the json object which will manage the information received
+        GsonBuilder builder = new GsonBuilder();
+
+        // Register an adapter to manage the date types as long values
+        builder.registerTypeAdapter(Date.class, new JsonDeserializer<Date>() {
+            public Date deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+                return new Date(json.getAsJsonPrimitive().getAsLong());
+            }
+        });
+
+        gson = builder.create();
+
         final Service s = new Service() {
             @Override
             protected Task createTask() {
+                return new Task() {
+                    @Override
+                    protected Object call() throws Exception {
+                        String response = Request.httpGetJsonRequest(HttpConstants.HOSTNAME + "/vcubs");
+                        Type token = new TypeToken<Collection<Vcub>>(){}.getType();
+                        Collection<Vcub> resultVcubs = gson.fromJson(response, token);
 
+                        vcubs = new ArrayList<Vcub>(resultVcubs);
+
+                        response = Request.httpGetJsonRequest(HttpConstants.HOSTNAME + "/tranvias");
+                        token = new TypeToken<Collection<Vcub>>(){}.getType();
+                        Collection<TranviaVehiculo> resultTranvias = gson.fromJson(response, token);
+
+                        tranvias = new ArrayList<TranviaVehiculo>(resultTranvias);
+
+                        response = Request.httpGetJsonRequest(HttpConstants.HOSTNAME + "/mobibuses");
+                        token = new TypeToken<Collection<MoviBusVehiculo>>(){}.getType();
+                        Collection<MoviBusVehiculo> resultMobis = gson.fromJson(response, token);
+
+                        mobis = new ArrayList<MoviBusVehiculo>(resultMobis);
+
+                        return null;
+                    }
+                };
+            }
+
+            @Override
+            protected void succeeded() {
+                vcubsItems = FXCollections.observableArrayList(vcubs);
+                tranviasItems = FXCollections.observableArrayList(tranvias);
+                movisItems = FXCollections.observableArrayList(mobis);
             }
         };
+
+        s.start();
     }
-
-    public void addVcub(Vcub v) {
-
-    }
-
-
 }
